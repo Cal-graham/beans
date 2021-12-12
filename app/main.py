570d089ce4c36268bda8d8c15fd73a7a143776b7
WoJiaoCal99 <cal.graham707@gmail.com>
2021-12-12 11:04:38 -0500

    initial commit

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session, make_response
+from app import site_frame
+import json
+from time import time
+
+
+main_blueprint = Blueprint('main', __name__)
+
+
+@main_blueprint.route('/read', methods=['GET'])
+def read():
+    data = []
+    data.append(time() * 1000)
+    for x in range(4):
+        data.append(site_frame.current_read[x])
+    print(data)
+    response = make_response(json.dumps(data))
+    response.content_type = 'application/json'
+    return response
+
