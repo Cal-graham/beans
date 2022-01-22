e5aed4f8eb244f741147237e9f12f9ebd6f4e8fc
WoJiaoCal99 <cal.graham707@gmail.com>
2022-01-22 12:39:19 -0500

    troubleshooting

--- app/arduino_test.py
+++ app/arduino_test.py
@@ -5,12 +5,15 @@ import serial
 def run():
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
     while True:
-        ser.reset_input_buffer(); sleep(0.1)
+        ser.reset_input_buffer();
         value = 0
         while value == 0:
             if ser.in_waiting > 0:
-                ser.readline(); value = ser.readline()
+                ser.readline()
+                value = str(ser.readline()).replace("b'", '').replace(",\r\n", '')
+                value.split(',')
                 print(value)
+                print(int(value))
         sleep(1)
 
 
