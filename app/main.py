818864014f09c8e7a811006843c6e638db23a641
Cal-graham <cal.graham707@gmail.com>
2022-01-22 20:12:36 +0000

    bug fixes

--- app/main.py
+++ app/main.py
@@ -10,9 +10,9 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/read', methods=['GET'])
 def read():
     data = []
-    data.append(time() * 1000)
-    for x in range(4):
-        data.append(site_frame.current_read[x])
+    data.append(time() * 1000); [data.append(float(x)) for x in site_frame.analog_read()]
+    #for x in range(4):
+    #    data.append(site_frame.current_read[x])
     print(data)
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
