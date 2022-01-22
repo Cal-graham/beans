fc8ab55585b6ed9eea288fc52550cb236b905f59
WoJiaoCal99 <cal.graham707@gmail.com>
2022-01-22 11:47:46 -0500

    troubleshooting

--- app/arduino_test.py
+++ app/arduino_test.py
@@ -2,16 +2,18 @@ from time import sleep
 import serial
 
 
-ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-while True:
-    ser.reset_input_buffer()
-    value = 0
-    while value == 0:
-        if ser.in_waiting > 0:
-            value = ser.readline().decode('utf-8').rstrip()
-            print(value)
-    sleep(1)
+def run():
+    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+    while True:
+        ser.reset_input_buffer()
+        value = 0
+        while value == 0:
+            if ser.in_waiting > 0:
+                value = ser.readline().decode('utf-8').rstrip()
+                print(value)
+        sleep(1)
 
 
-
+if __name__ == "__main__":
+    run()
 
