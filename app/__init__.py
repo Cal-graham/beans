be92f5ac2b5f0ef2ecffd7ed941b29284cb1875b
Cal <45156674+Cal-graham@users.noreply.github.com>
2022-03-11 14:54:57 -0500

    Manual Revision

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, session, request, redirect, url_for
 from flask_session import Session
-import atexit
 from siteFrame import SiteFrame
+import atexit
 
 
 site_frame = SiteFrame()
@@ -10,24 +10,22 @@ site_frame = SiteFrame()
 def create_app():
     #   Create app instance
     app = Flask(__name__)
-    Session(app);
+    Session(app)
 
-    #   blueprint for main page
+    #   blueprint for all routes
     from main import main_blueprint
     app.register_blueprint(main_blueprint, url_prefix='/main')
 
-    #   reroute to main page
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
-        return redirect(url_for('main.read'))
-    
     return app
 
 
 if __name__ == "__main__":
     #   Run
-    app = create_app()
+    app_ = create_app()
+
+    @app_.route('/', methods=['GET', 'POST'])
+    def index():
+        return render_template('main.html')
 
     atexit.register(lambda: site_frame.exit())
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
+    app_.run(host='0.0.0.0', port=5000, debug=True)
