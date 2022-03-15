b1fd5bb1595a0e40fca01142b4bcd4fa0bbb47e0
Cal <45156674+Cal-graham@users.noreply.github.com>
2022-03-15 11:13:08 -0400

    Adding continuous read and smoothing

--- app/siteFrame.py
+++ app/siteFrame.py
@@ -1,6 +1,8 @@
 from time import sleep
 import threading
 import numpy as np
+from statsmodels.nonparametric.smoothers_lowess import lowess
+
 
 if 1:
     import Adafruit_ADS1x15
@@ -11,36 +13,46 @@ if 0:
 
 class SiteFrame:
     onRpi = onRpi
-    data = {}
+    raw_data = {}
+    real_data = {}
+    time_data = {}
+    filter_data = {}
     run_adc_thread = 0
     adc_thread = None
-    adc1 = None
-    adc2 = None
+    adc = []
     current_read = {}
     read_prev = {}
     read_avg = {}
-    pins2 = {
-        'pressure_br': 0,
-        'temp_gh': 1,
-        'temp_br': 2,
-        'gh4': 3
+    pins = {
+        'pressure_br': '2.0',
+        'temp_gh': '2.1',
+        'temp_br': '2.2',
+        #'gh4': 3
     }
-    pins1 = {
-        'br1': 0,
-        'br2': 1,
-        'br3': 2,
-        'br4': 3
-    }
+    '''pins1 = {
+        #'br1': 0,
+        #'br2': 1,
+        #'br3': 2,
+        #'br4': 3
+    }'''
 
     def __init__(self):
         if onRpi:
-            self.adc1 = Adafruit_ADS1x15.ADS1115()
-            self.adc2 = Adafruit_ADS1x15.ADS1115(address=0x49)
-            self.GAIN = 1; print('Site Frame Initialized')
+            self.adc.append(Adafruit_ADS1x15.ADS1115())
+            self.adc.append(Adafruit_ADS1x15.ADS1115(address=0x49))
+            self.GAIN = 1
+            print('Site Frame Initializing...')
+            for key in self.pins.keys():
+                self.raw_data[key] = []
+                self.real_data[key] = []
+                self.filter_data[key] = []
+                self.time_data[key] = []
         if self.run_adc_thread == 0:
             self.run_adc_thread = 1
             self.adc_thread = threading.Thread(target=lambda: self.comms())
             self.adc_thread.start()
+            sleep(5)
+            print('Initialized')
         else:
             pass
 
@@ -50,15 +62,21 @@ class SiteFrame:
         pass
 
     def comms(self):
-        read = {}
+        self.pull_readings(4)
+        while self.run_adc_thread:
+            self.pull_readings(0.5)
+            self.limit_datapoints(2000)
+            
+        '''read = {}
         for idx in range(10):
-            read = self.read_all_pins(); #print(read)
+            read = self.read_all_pins()
             for key in read.keys():
                 if key in self.data.keys():
                     self.data[key].append(read[key])
                 else:
                     self.data[key] = [read[key]]
-        self.current_read = read; self.read_prev = read;
+        self.current_read = read 
+        self.read_prev = read
         print(f'Initial Reading: {read}')
         while self.run_adc_thread:
             try:
@@ -74,8 +92,46 @@ class SiteFrame:
                 self.current_read = read
                 self.read_prev = self.current_read
             except Exception as e:
-                print(e)
+                print(e)'''
+        
+    def limit_datapoints(self, max):
+        for key in self.pins.keys():
+            if len(self.raw_data[key]) > max:
+                del self.raw_data[key][0:len(self.raw_data[key])-max]
                 
+    def pull_readings(self, seconds):
+        atime = seconds/len(self.pins.keys())
+        for key in self.pins.keys():
+            start = time()
+            controller = self.adc[self.pins[key].split('.')[0]]
+            pin = self.pins[key].split('.')[1]
+            controller.start_adc(pin, gain=self.GAIN)
+            while time() - start < atime:
+                self.raw_data[key].append(controller.get_last_result())
+                self.time_data[key].append(time(int(time()%60)))
+            controller.stop_adc()
+           
+    def update_data(self):
+        for key in self.pins.keys():
+            if 'temp' in key:
+                self.real_data[key] = self.convert_temp(self.raw_data[key])
+            if 'pressure' in key:
+                self.real_data[key] = self.convert_pressure(self.raw_data[key])
+            else:
+                self.real_data[key] = self.raw_data[key]
+   
+    def smoothing(self):
+        self.update_data()
+        for key in self.pins.keys():
+            self.filter_data[key] = lowess(self.real_data[key], self.time_data[key], frac=0.4)
+   
+    def pull_points(self):
+        self.smoothing()
+        results = {}
+        for key in self.pins.keys():
+            results[key] = self.filter_data[key][-1]
+        results['time'] = self.time_data['pressure_br']
+        
     def read_all_pins(self):
         result = {}
         read = self.analog_read()
@@ -115,7 +171,7 @@ class SiteFrame:
         adc.start_adc(pin, gain=self.GAIN)
         val = []
         while start - time.time() < self.atime:
-            val.append(adc.get_last_result)
+            val.append(adc.get_last_result())
         print(val)
         adc.stop_adc()
         return np.average(val)
