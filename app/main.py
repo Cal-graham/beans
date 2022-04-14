f4523983d6bfe27fd8aa032e4abf4d3a623670da
Cal-graham <cal.graham707@gmail.com>
2022-04-14 20:11:25 +0100

    Working version with drag-editable profiling

--- app/main.py
+++ app/main.py
@@ -43,9 +43,9 @@ def read():
     return response
 
 
-@main_blueprint.route('/profile_enable/<graph>', methods=['GET'])
-def profile_enable(graph):
-    site_frame.enable_profile(str(graph)); print(f'START: {site_frame.profile_generate}'); return '1'
+#@main_blueprint.route('/profile_enable/<graph>', methods=['GET'])
+#def profile_enable(graph):
+#    site_frame.enable_profile(str(graph)); print(f'START: {site_frame.profile_generate}'); return '1'
     #data = site_frame.filter_data #site_frame.pull_points()
     #now = datetime.now()
     #data['time'] = now.strftime("%S") #data.append(now.strftime("%S"))
@@ -55,22 +55,22 @@ def profile_enable(graph):
     #return response
 
 
-@main_blueprint.route('/profile_disable', methods=['GET'])
-def profile_disable():
-    site_frame.disable_profile(); print(f'END: {site_frame.profile_generate}'); return '1'
+#@main_blueprint.route('/profile_disable', methods=['GET'])
+#def profile_disable():
+#    site_frame.disable_profile(); print(f'END: {site_frame.profile_generate}'); return '1'
 
 
-@main_blueprint.route('/profile_start', methods=['GET'])
-def profile_start():
-    site_frame.start_profile(); print(f'START: {site_frame.profile_generate}'); return '1'
+@main_blueprint.route('/start_profiling', methods=['GET'])
+def start_profiling():
+    site_frame.start_time = time(); return '1'
 
 
-@main_blueprint.route('/current_profiles', methods=['GET'])
-def current_profiles():
-    data = [site_frame.current_profiles[key] for key in site_frame.current_profiles.keys()]
-    response = make_response(json.dumps(data)); #print(data)
-    response.content_type = 'application/json'; #print(f'END: {time()}')
-    return response
+#@main_blueprint.route('/current_profiles', methods=['GET'])
+#def current_profiles():
+#    data = [site_frame.current_profiles[key] for key in site_frame.current_profiles.keys()]
+#    response = make_response(json.dumps(data)); #print(data)
+#    response.content_type = 'application/json'; #print(f'END: {time()}')
+#    return response
 
 
 @main_blueprint.route('/custom_profile_settings/<id>', methods=['GET'])
@@ -81,30 +81,30 @@ def custom_profile_settings(id):
     return response
 
 
-@main_blueprint.route('/profile_change/<graph>/<source>/<id>/<X>', methods=['GET'])
-@main_blueprint.route('/profile_change/<graph>/<source>/<id>', methods=['GET'])
-def profile_change(graph, source, id, X=-1000):
-    if X != -1000:
-        site_frame.profile_generate[id] = X; #print(site_frame.profile_generate)
-    for key in site_frame.current_profiles.keys():
-        if str(graph) in key:
-            if str(source) in key:
-                site_frame.current_profiles[key] = str(id)
-    return '1'
+#@main_blueprint.route('/profile_change/<graph>/<source>/<id>/<X>', methods=['GET'])
+#@main_blueprint.route('/profile_change/<graph>/<source>/<id>', methods=['GET'])
+#def profile_change(graph, source, id, X=-1000):
+#    if X != -1000:
+#        site_frame.profile_generate[id] = X; #print(site_frame.profile_generate)
+#    for key in site_frame.current_profiles.keys():
+#        if str(graph) in key:
+#            if str(source) in key:
+#                site_frame.current_profiles[key] = str(id)
+#    return '1'
 
 
-@main_blueprint.route('/live_graph')
-def live_graph():
-    HTML_args = {}
-    HTML_args['graphs'] = []; HTML_args['source'] = {}
-    for key in site_frame.pins.keys():
-        if key.split('_')[0] not in HTML_args['graphs']:
-            HTML_args['graphs'].append(key.split('_')[0])
-            HTML_args['source'][key.split('_')[0]] = []
-        HTML_args['source'][key.split('_')[0]].append(key.split('_')[1])
-        #else:
-        #    HTML_args['streams'][key.split('_')[0]] += 1
-    return render_template('live_graph.html', args=HTML_args)
+#@main_blueprint.route('/live_graph')
+#def live_graph():
+#    HTML_args = {}
+#    HTML_args['graphs'] = []; HTML_args['source'] = {}
+#    for key in site_frame.pins.keys():
+#        if key.split('_')[0] not in HTML_args['graphs']:
+#            HTML_args['graphs'].append(key.split('_')[0])
+#            HTML_args['source'][key.split('_')[0]] = []
+#        HTML_args['source'][key.split('_')[0]].append(key.split('_')[1])
+#        #else:
+ #       #    HTML_args['streams'][key.split('_')[0]] += 1
+ #   return render_template('live_graph.html', args=HTML_args)
 
 
 @main_blueprint.route('/current_custom_profiling/<graph>/<source>', methods=['GET'])
@@ -114,11 +114,13 @@ def current_custom_profiling(graph, source):
     return response
 
 
-@main_blueprint.route('/submit_custom_profile/<graph>/<source>/<xdat>/<ydat>', methods=['GET'])
-def submit_custom_profile(graph, source, xdat, ydat):
-    xdata = [float(x) for x in str(xdat).split(',') if x]; print(xdat)
-    ydata = [float(y) for y in str(ydat).split(',') if y]
-    site_frame.profiles.custom_profiles[str(graph)][str(source)] = [xdata,ydata]
-    site_frame.profiles.gen_custom_profile_functions()
-    site_frame.current_profiles[str(graph)+'_'+str(source)] = 'custom_'+str(graph)+'_'+str(source)
+@main_blueprint.route('/submit_custom_profile/<graph>/<xdat>/<ydat>', methods=['GET'])
+def submit_custom_profile(graph, xdat, ydat):
+    keys = []
+    for key in site_frame.profiles.custom_profiles[str(graph)].keys():
+        keys.append(key)
+    for idx in range(len(keys)):
+        xdata = [float(x) for x in str(xdat).split('|')[idx].split(',') if x]; print(xdat)
+        ydata = [float(y) for y in str(ydat).split('|')[idx].split(',') if y]
+        site_frame.profiles.custom_profiles[str(graph)][keys[idx]] = [xdata,ydata]
     return '1'
